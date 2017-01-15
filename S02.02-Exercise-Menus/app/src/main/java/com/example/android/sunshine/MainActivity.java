5a6139ff9ccb66e7be68908246e21a87c5ee479f
gohv <hristovosotirovgeorgi@gmail.com>
2017-01-15 17:20:23 +0200

    Not empty commit message

--- S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -18,7 +18,11 @@ package com.example.android.sunshine;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.utilities.NetworkUtils;
@@ -29,6 +33,15 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forecast,menu);
+
+        return true;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,12 +110,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (2) Create a menu resource in res/menu/ called forecast.xml
-    // TODO (3) Add one item to the menu with an ID of action_refresh
-    // TODO (4) Set the title of the menu item to "Refresh" using strings.xml
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_refresh:
+                mWeatherTextView.setText("");
+                loadWeatherData();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-    // TODO (5) Override onCreateOptionsMenu to inflate the menu for this Activity
-    // TODO (6) Return true to display the menu
+    // completed (2) Create a menu resource in res/menu/ called forecast.xml
+    // completed (3) Add one item to the menu with an ID of action_refresh
+    // completed (4) Set the title of the menu item to "Refresh" using strings.xml
 
-    // TODO (7) Override onOptionsItemSelected to handle clicks on the refresh button
+    // completed (5) Override onCreateOptionsMenu to inflate the menu for this Activity
+    // completed (6) Return true to display the menu
+
+    // completed (7) Override onOptionsItemSelected to handle clicks on the refresh button
 }
